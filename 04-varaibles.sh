52aff6ba9c90a01ff1d3e52179da78a1301d90a0
kishorekumar <kishorevabanagiri@gmail.com>
2025-09-19 12:55:41 +0530

    hello hyderabad started

---
+++ 04-varaibles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+person1=$1
+person2=$2
+
+
+
+echo "$person1:: hello $person2"
+echo "$person2 :: hii $person1"
+echo "$person1:: whats upp"
+echo "$person2 :: nothing whats upp"
+echo "$person1:: nope"
