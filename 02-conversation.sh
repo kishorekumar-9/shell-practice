9d241f893d013ad87e83e7fbd39b554139359d3b
kishorekumar <kishorevabanagiri@gmail.com>
2025-09-20 10:33:21 +0530

    special vars

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+echo "arun raj:: hello zion abraham"
+echp "zion zbraham:: hii arun raj"
+echo "arun raj:: whats upp"
+echo "zion abraham:: nothing whats upp"
+echo "arun raj:: nope"
 
