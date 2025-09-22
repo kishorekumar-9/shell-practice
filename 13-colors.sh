6be348cada047de32b87eaa2677f7fb128d717e6
kishorekumar <kishorevabanagiri@gmail.com>
2025-09-22 11:28:15 +0530

    special vars

---
+++ 13-colors.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo "$R ERROR:: Please run this script with root access $N"
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access"
+fi
+
+#validate function takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "Installing $2 is ... $G SUCCESS $N"
+    else
+        echo -e "Insatlling $2 is ... $R FAILURE $N"
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo -e "Nothing to do MYSQL... $Y already installed $N"
+fi  
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "Python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "Python3"
+else
+    echo -e "Nothing to do python... $Y already installed $N"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "Nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else    
+    echo -e "Nothing to do nginx... $y already installed $N"
+fi
