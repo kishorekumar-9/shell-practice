70fe73d67cf6c9cc501218ed89921b8b04299a0f
kishorekumar <kishorevabanagiri@gmail.com>
2025-09-26 09:18:44 +0530

    special vars

--- 16-loops.sh
+++ 16-loops.sh
@@ -8,9 +8,10 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-Packages=("mysql" "python" "nginx" "httpd")
+PACKAGES=("mysql" "python3" "nginx" "httpd")
+
 mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
